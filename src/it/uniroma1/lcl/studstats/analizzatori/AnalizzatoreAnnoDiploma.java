e6d0739fe15cb652c96d6164a42a6a86b8fcead3
Valerio Pescatori <pescatori.1807838@studenti.uniroma1.it>
2018-05-12 16:59:07 +0200

    Update .gitignore

--- src/it/uniroma1/lcl/studstats/analizzatori/AnalizzatoreAnnoDiploma.java
+++ src/it/uniroma1/lcl/studstats/analizzatori/AnalizzatoreAnnoDiploma.java
@@ -18,8 +18,11 @@ public class AnalizzatoreAnnoDiploma implements Analizzatore
 	}
 
 	@Override
-	public Rapporto generaRapporto(Collection<Studente> studs) { return new Rapporto(Map.of("ANNI_DIPLOMA",
-			studs.stream().map(x -> x.get("Anno Diploma")).sorted(Comparator.reverseOrder())
-			.collect(Collectors.groupingBy(Function.identity(), LinkedHashMap::new, Collectors.counting()))),
-			PossibiliRapporti.ANNO_DIPLOMA); }
+	public Rapporto generaRapporto(Collection<Studente> studs)
+	{
+		return new Rapporto(Map.of("ANNI_DIPLOMA",
+				studs.stream().map(x -> x.get("Anno Diploma")).sorted(Comparator.reverseOrder())
+						.collect(Collectors.groupingBy(Function.identity(), LinkedHashMap::new, Collectors.counting()))),
+				PossibiliRapporti.ANNO_DIPLOMA);
+	}
 }
